94e5034711ca3b65e4dbb0079750b45c05e1ad67
NickUfer <me@nick-ufer.de>
2019-09-23 13:10:24 +0200

    Fixes formatting
    Adds licensing headers

--- examples/src/main/java/com/inwx/domrobot/DomaincheckWithCustomModelsExample.java
+++ examples/src/main/java/com/inwx/domrobot/DomaincheckWithCustomModelsExample.java
@@ -1,15 +1,32 @@
+/*
+ * Copyright (c) 2014 - 2019 INWX GmbH & Co. KG
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package com.inwx.domrobot;
+
+import java.util.List;
 
 import com.google.gson.annotations.SerializedName;
 import com.inwx.domrobot.model.BasicResponse;
 import com.inwx.domrobot.model.GenericResponse;
 
-import java.util.List;
-
 public class DomaincheckWithCustomModelsExample {
 
     public static void main(String[] args) throws Exception {
-
         String username = "";
         String password = "";
         String sharedSecret = ""; // Only necessary if 2FA is enabled.
@@ -17,12 +34,12 @@ public class DomaincheckWithCustomModelsExample {
 
         ApiClient client = new ApiClient(ApiClient.OTE_URL, true);
 
-        // By default the ApiClient uses the test api (OT&E). If you want to use the production/live api
-        // we have a constant named LIVE_URL in the ApiClient class you can use.
+        // By default the ApiClient uses the test api (OT&E). If you want to use the production/live
+        // api we have a constant named LIVE_URL in the ApiClient class you can use.
         BasicResponse loginResponse = client.login(username, password, sharedSecret);
         if (!loginResponse.wasSuccessful()) {
-            throw new Exception("Api login error. Code: " + loginResponse.getCode()
-                    + ", Message: " + loginResponse.getMsg());
+            throw new Exception("Api login error. Code: " + loginResponse.getCode() + ", Message: "
+                            + loginResponse.getMsg());
         }
 
         RequestModel request = new RequestModel(domain);
@@ -30,8 +47,8 @@ public class DomaincheckWithCustomModelsExample {
 
         if (!response.wasSuccessful()) {
             client.logout();
-            throw new Exception("Api error while checking the domain status. Code: " + response.getCode()
-                    + ", Message: " + response.getMsg());
+            throw new Exception("Api error while checking the domain status. Code: "
+                            + response.getCode() + ", Message: " + response.getMsg());
         }
 
         if (response.getDomains().get(0).isAvailable()) {
@@ -60,10 +77,12 @@ public class DomaincheckWithCustomModelsExample {
     public static class ResponseModel extends GenericResponse<ResponseModel.ResData> {
 
         public static class ResData {
+
             @SerializedName("domain")
             private List<Domain> domains;
 
             public static class Domain {
+
                 @SerializedName("domain")
                 private String name;
                 @SerializedName("avail")
@@ -96,4 +115,3 @@ public class DomaincheckWithCustomModelsExample {
         }
     }
 }
-
