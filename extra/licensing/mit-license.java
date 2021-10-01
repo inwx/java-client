7ed23ea522611b47de97456e3cc1f4cf3e2c405d
Nick Ufer <nick@ufer.dev>
2021-10-01 21:40:02 +0200

    fix: removes space between time range for license generator template as it triggers bugs

--- extra/licensing/mit-license.java
+++ extra/licensing/mit-license.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 - $YEAR INWX GmbH & Co. KG
+ * Copyright (c) 2014-$YEAR INWX GmbH & Co. KG
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
